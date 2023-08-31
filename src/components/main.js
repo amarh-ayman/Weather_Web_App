49778dd658364e199ef7de842d2508e42197ab96
Amarah Bishtawi <63208748+amarh-ayman@users.noreply.github.com>
2023-08-31 12:27:41 +0300

    Update main.js
    reject unnecessary/unused code!

--- src/components/main.js
+++ src/components/main.js
@@ -1,23 +1,10 @@
-import React, { useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import Search from "./search";
 import WeatherDisplay from "./WeatherDisplay";
 import SideBar from "./sideBar";
-import { useWeatherContext } from "./WeatherContext";
+
 
 function Main() {
-  const { location, data, setData } = useWeatherContext();
-
-  useEffect(() => {
-    if (location) {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`;
-
-      axios.get(url).then((response) => {
-        setData(response.data);
-      });
-    }
-  }, [location, setData]);
-
   return (
     <React.Fragment>
       <Search />
